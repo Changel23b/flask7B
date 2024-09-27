42560b7fc8588322ea09650924bc5a621759a658
Changel23b <joseangelperez667@gmail.com>
2024-09-27 17:42:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import mysql.connector
 import datetime
 import pytz
 
+# Conexión a la base de datos
 con = mysql.connector.connect(
   host="185.232.14.52",
   database="u760464709_tst_sep",
@@ -13,22 +14,25 @@ con = mysql.connector.connect(
 
 app = Flask(__name__)
 
+# Ruta principal para cargar el HTML
 @app.route("/")
 def index():
     return render_template("app.html")
 
+# Ruta para buscar los contactos en la base de datos
 @app.route("/buscar")
 def buscar():
     if not con.is_connected():
         con.reconnect()
     cursor = con.cursor()
-    cursor.execute("SELECT * FROM tst0_cursos_pagos")
+    cursor.execute("SELECT * FROM tst0_contacto")
     
     registros = cursor.fetchall()
     con.close()
 
     return {"data": registros}
 
+# Ruta para registrar un nuevo contacto
 @app.route("/registrar", methods=["GET"])
 def registrar():
     args = request.args
@@ -37,13 +41,18 @@ def registrar():
         con.reconnect()
     cursor = con.cursor()
 
-    sql = "INSERT INTO tst0_cursos_pagos (Telefono, Archivo) VALUES (%s, %s)"
-    val = (args["telefono"], args["archivo"])
+    # Insertar nuevo registro en la tabla de contactos
+    sql = "INSERT INTO tst0_contacto (Correo_Electronico, Nombre, Asunto) VALUES (%s, %s, %s)"
+    val = (args["correo_electronico"], args["nombre"], args["asunto"])
     cursor.execute(sql, val)
     
     con.commit()
+
+    # Obtener el último ID insertado
+    contacto_id = cursor.lastrowid
     con.close()
 
+    # Configuración de Pusher
     pusher_client = pusher.Pusher(
         app_id = "1872169",
         key = "6ffe9987dac447a007d3",
@@ -52,14 +61,18 @@ def registrar():
         ssl=True
     )
 
-    curso_pago = {
-        "Id_Curso_Pago": cursor.lastrowid,
-        "Telefono": args["telefono"],
-        "Archivo": args["archivo"]
+    # Crear el objeto contacto
+    contacto = {
+        "Id_Contacto": contacto_id,
+        "Correo_Electronico": args["correo_electronico"],
+        "Nombre": args["nombre"],
+        "Asunto": args["asunto"]
     }
 
-    pusher_client.trigger("canalCursosPagos", "registroCursoPago", curso_pago)
-    return curso_pago
+    # Disparar el evento "registroContacto" con el nuevo contacto
+    pusher_client.trigger("canalContactos", "registroContacto", contacto)
+
+    return contacto
 
 if __name__ == "__main__":
     app.run(debug=True)
