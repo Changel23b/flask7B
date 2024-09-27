d58911697a7d7d733ad738932d98438f12a3c43c
Changel23b <joseangelperez667@gmail.com>
2024-09-27 17:31:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,55 +4,46 @@ import mysql.connector
 import datetime
 import pytz
 
-
-# Configuración de la conexión a la base de datos
 con = mysql.connector.connect(
-    host="185.232.14.52",
-    database="tst0_contacto",
-    user="u760464709_tst_sep_usr",
-    password="dJ0CIAFF="
+  host="185.232.14.52",
+  database="u760464709_tst_sep",
+  user="u760464709_tst_sep_usr",
+  password="dJ0CIAFF="
 )
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    con.close()
     return render_template("app.html")
 
-# Ruta para obtener los contactos de la base de datos
 @app.route("/buscar")
 def buscar():
     if not con.is_connected():
         con.reconnect()
-
     cursor = con.cursor()
-    cursor.execute("SELECT * FROM tst0_contacto ORDER BY Id_Contacto DESC")
+    cursor.execute("SELECT * FROM tst0_cursos_pagos")
+    
     registros = cursor.fetchall()
-
     con.close()
 
-    return registros
+    return {"data": registros}
 
-# Ruta para registrar un nuevo contacto
 @app.route("/registrar", methods=["GET"])
 def registrar():
     args = request.args
 
     if not con.is_connected():
         con.reconnect()
-
     cursor = con.cursor()
 
-    # Insertamos el nuevo contacto en la base de datos
-    sql = "INSERT INTO tst0_contacto (Correo_Electronico, Nombre, Asunto) VALUES (%s, %s, %s)"
-    val = (args["correo"], args["nombre"], args["asunto"])
+    sql = "INSERT INTO tst0_cursos_pagos (Telefono, Archivo) VALUES (%s, %s)"
+    val = (args["telefono"], args["archivo"])
     cursor.execute(sql, val)
     
     con.commit()
     con.close()
 
-    # Configuración de Pusher para notificar el evento de nuevo contacto
     pusher_client = pusher.Pusher(
         app_id = "1872169",
         key = "6ffe9987dac447a007d3",
@@ -61,10 +52,14 @@ def registrar():
         ssl=True
     )
 
-    # Enviamos el evento de nuevo contacto
-    pusher_client.trigger("canalContactos", "nuevoContacto", args)
+    curso_pago = {
+        "Id_Curso_Pago": cursor.lastrowid,
+        "Telefono": args["telefono"],
+        "Archivo": args["archivo"]
+    }
 
-    return args
+    pusher_client.trigger("canalCursosPagos", "registroCursoPago", curso_pago)
+    return curso_pago
 
 if __name__ == "__main__":
     app.run(debug=True)
