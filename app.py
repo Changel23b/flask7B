209bdc05defa1869d4cf455080f4f2eccd2b2dc0
Changel23b <joseangelperez667@gmail.com>
2024-12-11 18:12:37 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,6 @@ import mysql.connector
 app = Flask(__name__)
 
 class ControladorContactos:
-    def __init__(self):
-        self.db_config = {
-            "host": "185.232.14.52",
-            "database": "u760464709_tst_sep",
-            "user": "u760464709_tst_sep_usr",
-            "password": "dJ0CIAFF="
-        }
-
-    def conectar_bd(self):
-        return mysql.connector.connect(self.db_config)
-
     def notificar_actualizacion(self, evento, data):
         pusher_client = pusher.Pusher(
             app_id='1872169',
@@ -27,7 +16,12 @@ class ControladorContactos:
         pusher_client.trigger("canalContactos", evento, data)
 
     def buscar(self):
-        con = self.conectar_bd()
+        con = mysql.connector.connect({
+            "host": "185.232.14.52",
+            "database": "u760464709_tst_sep",
+            "user": "u760464709_tst_sep_usr",
+            "password": "dJ0CIAFF="
+        })
         cursor = con.cursor(dictionary=True)
         cursor.execute("SELECT * FROM tst0_contacto")
         registros = cursor.fetchall()
@@ -35,7 +29,12 @@ class ControladorContactos:
         return make_response(jsonify({"data": registros}))
 
     def guardar(self, correo_electronico, nombre, asunto):
-        con = self.conectar_bd()
+        con = mysql.connector.connect({
+            "host": "185.232.14.52",
+            "database": "u760464709_tst_sep",
+            "user": "u760464709_tst_sep_usr",
+            "password": "dJ0CIAFF="
+        })
         cursor = con.cursor()
         sql = """
         INSERT INTO tst0_contacto (Correo_Electronico, Nombre, Asunto)
@@ -57,7 +56,12 @@ class ControladorContactos:
         return make_response(jsonify(contacto))
 
     def modificar(self, id_contacto, correo_electronico, nombre, asunto):
-        con = self.conectar_bd()
+        con = mysql.connector.connect({
+            "host": "185.232.14.52",
+            "database": "u760464709_tst_sep",
+            "user": "u760464709_tst_sep_usr",
+            "password": "dJ0CIAFF="
+        })
         cursor = con.cursor()
         sql = """
         UPDATE tst0_contacto SET Correo_Electronico = %s, Nombre = %s, Asunto = %s
@@ -78,7 +82,12 @@ class ControladorContactos:
         return make_response(jsonify(contacto))
 
     def eliminar(self, id_contacto):
-        con = self.conectar_bd()
+        con = mysql.connector.connect({
+            "host": "185.232.14.52",
+            "database": "u760464709_tst_sep",
+            "user": "u760464709_tst_sep_usr",
+            "password": "dJ0CIAFF="
+        })
         cursor = con.cursor()
         sql = "DELETE FROM tst0_contacto WHERE Id_Contacto = %s"
         val = (id_contacto,)
